3fe104c9fc28236edfe389377ec6d4d814f7ccee
Mohammad Rashid <dopplesoldner@gmail.com>
2014-07-26 17:07:47 +0100

    assignment 1

---
+++ PGM-Programming_Assignment_1/ComputeMarginal.m
@@ -0,0 +1,49 @@
+%ComputeMarginal Computes the marginal over a set of given variables
+%   M = ComputeMarginal(V, F, E) computes the marginal over variables V
+%   in the distribution induced by the set of factors F, given evidence E
+%
+%   M is a factor containing the marginal over variables V
+%   V is a vector containing the variables in the marginal e.g. [1 2 3] for
+%     X_1, X_2 and X_3.
+%   F is a vector of factors (struct array) containing the factors 
+%     defining the distribution
+%   E is an N-by-2 matrix, each row being a variable/value pair. 
+%     Variables are in the first column and values are in the second column.
+%     If there is no evidence, pass in the empty matrix [] for E.
+
+
+function M = ComputeMarginal(V, F, E)
+
+% Check for empty factor list
+if (numel(F) == 0)
+      warning('Warning: empty factor list');
+      M = struct('var', [], 'card', [], 'val', []);      
+      return;
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% YOUR CODE HERE:
+% M should be a factor
+% Remember to renormalize the entries of M!
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% compute joint CPD
+cpd = ComputeJointDistribution(F);
+
+% observe evidence
+observed = ObserveEvidence(cpd, E);
+
+% re-normalize
+observed.val = observed.val ./ sum(observed.val);
+
+% get variable which has to be marginalized
+var_to_marginalize = setdiff(observed.var, V);
+
+% calculate marginal
+M = FactorMarginalization(observed, var_to_marginalize);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+M = StandardizeFactors(M);
+
+end
